81d14343fa0b91cb54772c0345ea077fbd555e2d
Florian <f.muellerklein@gmail.com>
2015-01-09 09:49:49 -0500

    Update gbt.features.py
    added dummy encoding to categorical variables

--- gbt.features.py
+++ gbt.features.py
@@ -5,6 +5,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 train_loc = 'train.csv'
 test_loc = 'test.csv'
+TREES = 30
+NODES = 7
 
 def get_leaf_indices(ensemble, x):
     x = x.astype(np.float32)
@@ -30,15 +32,17 @@ def clean(data):
     
     return data
     
-def load_fit_data(path):
+def load_fit_data(path, gbt):
     fit_x = pd.read_csv(path)
     fit_x = clean(fit_x)
     fit_y = fit_x['Survived'].astype(int).values
     fit_x = fit_x.drop('Survived', 1)
 		
-    fit_x = fit_x.values
+    fit_x = pd.get_dummies(fit_x).values
+    
+    gbt.fit(fit_x, fit_y)
 	
-    return fit_x, fit_y
+    return gbt
 
 def vw_ready(data):
 	data[data == 0] = -1
@@ -46,74 +50,55 @@ def vw_ready(data):
 	
 	return data
 
-def load_train_vw(path, gbt):
+def load_data(path, gbt, train):
     reader = pd.read_csv(path, chunksize = 100)
     for chunk in reader:
-        chunk = clean(chunk)
-        survived = chunk['Survived'].astype(int)
-        chunk = chunk.drop('Survived', 1)
-        chunk = chunk.drop('PassengerId', 1)
+        if train == True:
+            chunk = clean(chunk)
+            y = chunk['Survived'].astype(int)
+            chunk = chunk.drop('Survived', 1)
+            chunk = chunk.drop('PassengerId', 1)
+            y = vw_ready(y)
+        else:
+            chunk = clean(chunk)
+            y = chunk['PassengerId']
+            chunk = chunk.drop('PassengerId', 1)
+            y = (y.astype(str) + ' |C').as_matrix()
+        
         orig = []
         for colname in list(chunk.columns.values):
             orig.append(colname + chunk[colname].astype(str))
         
-        chunk = chunk.values
         orig = np.column_stack(orig)
 
-        gbt_tree = get_leaf_indices(gbt, chunk).astype(str)
-        for row in range(0, chunk.shape[0]):
-            for column in range(0,500,1):
-                gbt_tree[row,column] = ('C' + str(column) + str(gbt_tree[row, column]))
-        
-        survived = vw_ready(survived)
-        
-        out = np.column_stack((survived, orig, gbt_tree))
-        
-        file_handle = file('tree.train.txt', 'a')
-        np.savetxt(file_handle, out, delimiter = ' ', fmt = '%s')
-        file_handle.close()
-        
-def load_test_vw(path, gbt):
-    reader = pd.read_csv(path, chunksize = 100)
-    for chunk in reader:
-        chunk = clean(chunk)
-        pid = chunk['PassengerId']
-        chunk = chunk.drop('PassengerId', 1)
-        orig = []
-        for colname in list(chunk.columns.values):
-            orig.append(colname + chunk[colname].astype(str))
-        
+        gbt_tree = get_leaf_indices(gbt, pd.get_dummies(chunk).values).astype(str)
         chunk = chunk.values
-        orig = np.column_stack(orig)
-
-        gbt_tree = get_leaf_indices(gbt, chunk).astype(str)
         for row in range(0, chunk.shape[0]):
-            for column in range(0,500,1):
-                gbt_tree[row,column] = ('C' + str(column) + str(gbt_tree[row, column]))
+            for column in range(0, TREES, 1):
+                gbt_tree[row,column] = ('T' + str(column) + str(gbt_tree[row, column]))
         
-        pid = (pid.astype(str) + ' |C').as_matrix()
         
-        out = np.column_stack((pid, orig, gbt_tree))
+        out = np.column_stack((y, orig, gbt_tree))
         
-        file_handle = file('tree.test.txt', 'a')
-        np.savetxt(file_handle, out, delimiter = ' ', fmt = '%s')
-        file_handle.close()
-
-	
+        if train == True:
+            file_handle = file('tree.train.txt', 'a')
+            np.savetxt(file_handle, out, delimiter = ' ', fmt = '%s')
+            file_handle.close()
+        else:
+            file_handle = file('tree.test.txt', 'a')
+            np.savetxt(file_handle, out, delimiter = ' ', fmt = '%s')
+            file_handle.close()
+            
 def main():
-    gbt = GradientBoostingClassifier(n_estimators = 500, max_depth = 7, verbose = 1)
-	
-    fit_x, fit_y = load_fit_data(train_loc)
+    gbt = GradientBoostingClassifier(n_estimators = TREES, max_depth = NODES, verbose = 1)
     
-    gbt.fit(fit_x, fit_y)
-    fit_x = None
-    fit_y = None
+    gbt = load_fit_data(train_loc, gbt)
     
     print('transforming and writing training data ... ')
-    load_train_vw(train_loc, gbt)
+    load_data(train_loc, gbt, train = True)
     
     print('transforming and writing testing data ... ')
-    load_test_vw(test_loc, gbt)
+    load_data(test_loc, gbt, train = False)
     
 if __name__ == '__main__':
     main()
